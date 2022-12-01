d313ef36b97a931ab9926d2f7c80ddf8ec3db52d
mqqx <aoc@mqqx.dev>
2022-12-01 15:00:03 +0700

    ✨ Add Feature * Add working solution for second challenge of day 1 * Refactor code to be more readable

--- src/main/java/dev/hmmr/aoc/days/Day1.java
+++ src/main/java/dev/hmmr/aoc/days/Day1.java
@@ -1,8 +1,12 @@
 package dev.hmmr.aoc.days;
 
 import static java.util.Arrays.stream;
+import static java.util.Comparator.reverseOrder;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
@@ -10,23 +14,48 @@ import org.springframework.core.io.Resource;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class Day1 {
+
+  public static final int NUMBER_OF_ELVES_CARRYING_THE_MOST_CALORIES = 3;
+
   @SneakyThrows
   static int calculateMostCalories(Resource example) {
-    final List<String> content =
-        stream(new String(example.getInputStream().readAllBytes()).split("\n\n")).toList();
+    final List<String> groupsOfCalories = splitIntoGroupsOfCalories(example);
 
     int mostCalories = 0;
 
-    for (String numbers : content) {
-      final Integer calories =
-          stream(numbers.split("\n")).toList().stream()
-              .map(Integer::valueOf)
-              .reduce(0, Integer::sum);
-      if (calories > mostCalories) {
-        mostCalories = calories;
+    for (String calories : groupsOfCalories) {
+      final Integer calorieCount = getCalorieCount(calories);
+
+      if (calorieCount > mostCalories) {
+        mostCalories = calorieCount;
       }
     }
 
     return mostCalories;
+  }
+
+  @SneakyThrows
+  static int calculateTotalOfTop3MostCalories(Resource example) {
+    final List<String> groupOfCalories = splitIntoGroupsOfCalories(example);
+
+    final ArrayList<Integer> calorieList =
+        groupOfCalories.stream()
+            .map(Day1::getCalorieCount)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    return calorieList.stream()
+        .sorted(reverseOrder())
+        .limit(NUMBER_OF_ELVES_CARRYING_THE_MOST_CALORIES)
+        .reduce(0, Integer::sum);
+  }
+
+  private static List<String> splitIntoGroupsOfCalories(Resource example) throws IOException {
+    return stream(new String(example.getInputStream().readAllBytes()).split("\n\n")).toList();
+  }
+
+  private static Integer getCalorieCount(String calories) {
+    return stream(calories.split("\n")).toList().stream()
+        .map(Integer::valueOf)
+        .reduce(0, Integer::sum);
   }
 }
--- src/test/java/dev/hmmr/aoc/days/Day1Test.java
+++ src/test/java/dev/hmmr/aoc/days/Day1Test.java
@@ -10,15 +10,29 @@ class Day1Test {
 
   @Test
   void testMostCaloriesExample() {
-    final Resource example = new ClassPathResource("1-example");
+    final Resource example = new ClassPathResource("day1/1-example");
 
     assertThat(Day1.calculateMostCalories(example)).isEqualTo(24_000);
   }
 
   @Test
   void testMostCalories() {
-    final Resource example = new ClassPathResource("1");
+    final Resource example = new ClassPathResource("day1/1");
 
     assertThat(Day1.calculateMostCalories(example)).isEqualTo(69_281);
+  }
+
+  @Test
+  void testTop3MostCaloriesExample() {
+    final Resource example = new ClassPathResource("day1/1-example");
+
+    assertThat(Day1.calculateTotalOfTop3MostCalories(example)).isEqualTo(45_000);
+  }
+
+  @Test
+  void testTop3MostCalories() {
+    final Resource example = new ClassPathResource("day1/1");
+
+    assertThat(Day1.calculateTotalOfTop3MostCalories(example)).isEqualTo(201_524);
   }
 }
