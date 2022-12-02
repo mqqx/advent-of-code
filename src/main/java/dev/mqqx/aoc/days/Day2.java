5808281687f9e75efb318337d2e5791730be26ed
mqqx <aoc@mqqx.dev>
2022-12-02 13:07:30 +0700

    🛠 Refactoring Code * Refactor code by removing duplicates to be more readable

--- src/main/java/dev/mqqx/aoc/days/Day2.java
+++ src/main/java/dev/mqqx/aoc/days/Day2.java
@@ -11,26 +11,21 @@ import org.springframework.core.io.Resource;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class Day2 {
+  private static final String ROCK_ROCK = "A X";
+  private static final String ROCK_SCISSORS = "A Z";
+  private static final String ROCK_PAPER = "A Y";
+  private static final String PAPER_ROCK = "B X";
+  private static final String PAPER_PAPER = "B Y";
+  private static final String SCISSORS_ROCK = "C X";
+  private static final String SCISSORS_PAPER = "C Y";
+  private static final String SCISSORS_SCISSORS = "C Z";
+  private static final String PAPER_SCISSORS = "B Z";
+
   @SneakyThrows
   static int calculateScore(Resource strategyGuide) {
     List<String> games = splitIntoGroupsOfGames(strategyGuide);
 
-    int points = 0;
-    for (String game : games) {
-      switch (game) {
-        case "A X" -> points += 1 + 3;
-        case "A Y" -> points += 2 + 6;
-        case "A Z" -> points += 3;
-        case "B X" -> points += 1;
-        case "B Y" -> points += 2 + 3;
-        case "B Z" -> points += 3 + 6;
-        case "C X" -> points += 1 + 6;
-        case "C Y" -> points += 2;
-        case "C Z" -> points += 3 + 3;
-      }
-    }
-
-    return points;
+    return calculatePoints(games);
   }
 
   @SneakyThrows
@@ -40,34 +35,34 @@ public class Day2 {
             .map(
                 game ->
                     switch (game) {
-                      case "A X" -> "A Z";
-                      case "A Y" -> "A X";
-                      case "A Z" -> "A Y";
-                      case "B X" -> "B X";
-                      case "B Y" -> "B Y";
-                      case "B Z" -> "B Z";
-                      case "C X" -> "C Y";
-                      case "C Y" -> "C Z";
-                      case "C Z" -> "C X";
-                      default -> "";
+                      case ROCK_ROCK -> ROCK_SCISSORS;
+                      case ROCK_PAPER -> ROCK_ROCK;
+                      case ROCK_SCISSORS -> ROCK_PAPER;
+                      case SCISSORS_ROCK -> SCISSORS_PAPER;
+                      case SCISSORS_PAPER -> SCISSORS_SCISSORS;
+                      case SCISSORS_SCISSORS -> SCISSORS_ROCK;
+                      default -> game;
                     })
             .toList();
 
+    return calculatePoints(games);
+  }
+
+  private static int calculatePoints(List<String> games) {
     int points = 0;
     for (String game : games) {
       switch (game) {
-        case "A X" -> points += 1 + 3;
-        case "A Y" -> points += 2 + 6;
-        case "A Z" -> points += 3;
-        case "B X" -> points += 1;
-        case "B Y" -> points += 2 + 3;
-        case "B Z" -> points += 3 + 6;
-        case "C X" -> points += 1 + 6;
-        case "C Y" -> points += 2;
-        case "C Z" -> points += 3 + 3;
+        case ROCK_ROCK -> points += 1 + 3;
+        case ROCK_PAPER -> points += 2 + 6;
+        case ROCK_SCISSORS -> points += 3;
+        case PAPER_ROCK -> points += 1;
+        case PAPER_PAPER -> points += 2 + 3;
+        case PAPER_SCISSORS -> points += 3 + 6;
+        case SCISSORS_ROCK -> points += 1 + 6;
+        case SCISSORS_PAPER -> points += 2;
+        case SCISSORS_SCISSORS -> points += 3 + 3;
       }
     }
-
     return points;
   }
 
