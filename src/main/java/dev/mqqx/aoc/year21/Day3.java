76f63a4f915a0eb3c2950085cdc2d93f09e3874f
mqqx <aoc@mqqx.dev>
2022-12-06 18:31:59 +0700

    🛠 Refactoring Code * Refactor day 3 solution to be better readable and use `^` (XOR) - operator instead of duplicated if-clauses

--- src/main/java/dev/mqqx/aoc/year21/Day3.java
+++ src/main/java/dev/mqqx/aoc/year21/Day3.java
@@ -14,7 +14,38 @@ public class Day3 {
 
   static int doThing(Resource input) {
     final List<String> strings = SplitUtils.lines(input).toList();
+    final int[] bitCounters = countBits(strings);
+    final String gammaRate = calculateGammaRate(strings, bitCounters);
+    final String epsilonRate = gammaRate.replace('0', '2').replace('1', '0').replace('2', '1');
 
+    return bitStringToInt(gammaRate) * bitStringToInt(epsilonRate);
+  }
+
+  static int doThingAdvanced(Resource input) {
+    int oxygenGeneratorRating = calculateRating(new ArrayList<>(SplitUtils.linesList(input)), true);
+    int co2ScrubberRating = calculateRating(new ArrayList<>(SplitUtils.linesList(input)), false);
+
+    return oxygenGeneratorRating * co2ScrubberRating;
+  }
+
+  private static int bitStringToInt(String gammaRate) {
+    return parseInt(gammaRate, 2);
+  }
+
+  private static String calculateGammaRate(List<String> strings, int[] bitCounters) {
+    final StringBuilder gammaRateBuilder = new StringBuilder();
+    for (int bitCounter : bitCounters) {
+      if (bitCounter >= strings.size() / 2) {
+        gammaRateBuilder.append("1");
+      } else {
+        gammaRateBuilder.append("0");
+      }
+    }
+
+    return gammaRateBuilder.toString();
+  }
+
+  private static int[] countBits(List<String> strings) {
     final int[] bitCounters = new int[strings.get(0).length()];
 
     for (String currentLine : strings) {
@@ -26,61 +57,38 @@ public class Day3 {
         }
       }
     }
-
-    final StringBuilder gammaRateBuilder = new StringBuilder();
-    final StringBuilder epsilonRateBuilder = new StringBuilder();
-    for (int bitCounter : bitCounters) {
-      if (bitCounter >= strings.size() / 2) {
-        gammaRateBuilder.append("1");
-        epsilonRateBuilder.append("0");
-      } else {
-        gammaRateBuilder.append("0");
-        epsilonRateBuilder.append("1");
-      }
-    }
-
-    return parseInt(gammaRateBuilder.toString(), 2) * parseInt(epsilonRateBuilder.toString(), 2);
-  }
-
-  static int doThingAdvanced(Resource input) {
-    int oxygenGeneratorRating = calculateRating(new ArrayList<>(SplitUtils.linesList(input)), true);
-    int co2ScrubberRating = calculateRating(new ArrayList<>(SplitUtils.linesList(input)), false);
-
-    return oxygenGeneratorRating * co2ScrubberRating;
+    return bitCounters;
   }
 
   private static int calculateRating(List<String> lines, boolean isMostRelevant) {
     final int[] bitCounters = new int[lines.get(0).length()];
     int currentIndex = 0;
     do {
-      for (String currentLine : lines) {
-        char[] charArray = currentLine.toCharArray();
-        char currentChar = charArray[currentIndex];
-        if ('1' == currentChar) {
-          bitCounters[currentIndex]++;
-        }
-      }
+      bitCounters[currentIndex] = countCurrentBits(lines, currentIndex);
 
-      if (currentIndex < bitCounters.length) {
-        final int finalCurrentIndex = currentIndex;
+      final boolean hasMostCommonBitOne = bitCounters[currentIndex] >= (float) lines.size() / 2;
+      final int finalCurrentIndex = currentIndex;
 
-        if (isMostRelevant) {
-          if (bitCounters[currentIndex] >= (float) lines.size() / 2) {
-            lines.removeIf(line -> line.charAt(finalCurrentIndex) == '0');
-          } else {
-            lines.removeIf(line -> line.charAt(finalCurrentIndex) == '1');
-          }
-        } else {
-          if (bitCounters[currentIndex] >= (float) lines.size() / 2) {
-            lines.removeIf(line -> line.charAt(finalCurrentIndex) == '1');
-          } else {
-            lines.removeIf(line -> line.charAt(finalCurrentIndex) == '0');
-          }
-        }
+      if (isMostRelevant ^ hasMostCommonBitOne) {
+        lines.removeIf(line -> line.charAt(finalCurrentIndex) == '0');
+      } else {
+        lines.removeIf(line -> line.charAt(finalCurrentIndex) == '1');
       }
       currentIndex++;
     } while (lines.size() > 1);
 
-    return parseInt(lines.get(0), 2);
+    return bitStringToInt(lines.get(0));
+  }
+
+  private static int countCurrentBits(List<String> lines, int currentIndex) {
+    int bitCounter = 0;
+    for (String currentLine : lines) {
+      char[] charArray = currentLine.toCharArray();
+      char currentChar = charArray[currentIndex];
+      if ('1' == currentChar) {
+        bitCounter++;
+      }
+    }
+    return bitCounter;
   }
 }
