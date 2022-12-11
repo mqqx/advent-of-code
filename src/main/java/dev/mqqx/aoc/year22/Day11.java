61244354713068cdd14575338eb75de1154f1559
mqqx <aoc@mqqx.dev>
2022-12-11 17:59:16 +0700

    🛠 Refactoring Code * Refactored parsing of monkey input as well as processing for better readability

--- src/main/java/dev/mqqx/aoc/year22/Day11.java
+++ src/main/java/dev/mqqx/aoc/year22/Day11.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Function;
+import java.util.function.LongFunction;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.core.io.Resource;
@@ -20,7 +20,7 @@ public class Day11 {
 
   record Monkey(
       List<Long> items,
-      Function<Long, Long> operation,
+      LongFunction<Long> operation,
       long testDivisibleBy,
       int trueMonkeyId,
       int falseMonkeyId,
@@ -35,36 +35,38 @@ public class Day11 {
   }
 
   private static long solve(Resource input, int numberOfRounds) {
-    List<Monkey> monkeys = getMonkeys(input);
-
+    final List<Monkey> monkeys = getMonkeys(input);
     final long gcp = monkeys.stream().map(Monkey::testDivisibleBy).reduce(1L, (a, b) -> a * b);
 
     for (int i = 0; i < numberOfRounds; i++) {
-
-      for (Monkey monkey : monkeys) {
-        if (monkey.items.isEmpty()) {
-          continue;
-        }
-
-        for (Long item : monkey.items) {
-          monkey.inspectionCounter.getAndIncrement();
-
-          long newItemLevel = monkey.operation.apply(item);
-          if (numberOfRounds <= 20) {
-            newItemLevel = (long) floor((double) newItemLevel / 3);
-          }
-
-          if (newItemLevel % monkey.testDivisibleBy == 0) {
-            monkeys.get(monkey.trueMonkeyId).items.add(newItemLevel % gcp);
-          } else {
-            monkeys.get(monkey.falseMonkeyId).items.add(newItemLevel % gcp);
-          }
-        }
-        monkey.items.clear();
-      }
+      processMonkeys(numberOfRounds, monkeys, gcp);
     }
 
     return multiplyHighestInspectionCounters(monkeys);
+  }
+
+  private static void processMonkeys(int numberOfRounds, List<Monkey> monkeys, long gcp) {
+    for (Monkey monkey : monkeys) {
+      if (monkey.items.isEmpty()) {
+        continue;
+      }
+
+      for (Long item : monkey.items) {
+        monkey.inspectionCounter.getAndIncrement();
+
+        long newItemLevel = monkey.operation.apply(item);
+        if (numberOfRounds <= 20) {
+          newItemLevel = (long) floor((double) newItemLevel / 3);
+        }
+
+        if (newItemLevel % monkey.testDivisibleBy == 0) {
+          monkeys.get(monkey.trueMonkeyId).items.add(newItemLevel % gcp);
+        } else {
+          monkeys.get(monkey.falseMonkeyId).items.add(newItemLevel % gcp);
+        }
+      }
+      monkey.items.clear();
+    }
   }
 
   private static long multiplyHighestInspectionCounters(List<Monkey> monkeys) {
@@ -78,68 +80,43 @@ public class Day11 {
   }
 
   private static List<Monkey> getMonkeys(Resource input) {
-    List<Monkey> monkeys = new ArrayList<>();
-
-    SplitUtils.lines(input, "\n\n")
-        .forEach(
+    return SplitUtils.lines(input, "\n\n")
+        .map(
             monkeyString -> {
               final List<String> monkeyLines = monkeyString.lines().toList();
 
               List<Long> items =
-                  Arrays.stream(monkeyLines.get(1).substring(18).split(","))
-                      .map(String::trim)
+                  Arrays.stream(monkeyLines.get(1).split(":")[1].trim().split(", "))
                       .map(Long::parseLong)
                       .toList();
 
-              final String[] operationString =
-                  monkeyLines
-                      .get(2)
-                      .substring("  Operation: new = old ".length())
-                      .trim()
-                      .split(" ");
+              final LongFunction<Long> operation = parseOperation(monkeyLines.get(2));
 
-              Function<Long, Long> operation;
+              final long testDivisibleBy = parseLong(monkeyLines.get(3).substring(21));
+              final int trueMonkeyId = parseInt(monkeyLines.get(4).substring(28).trim());
+              final int falseMonkeyId = parseInt(monkeyLines.get(5).substring(29).trim());
 
-              if ("*".equals(operationString[0])) {
-                try {
-                  final long number = parseLong(operationString[1]);
-                  operation = x -> x * number;
-                } catch (NumberFormatException e) {
-                  operation = x -> x * x;
-                }
-              } else {
-                try {
-                  final long number = parseLong(operationString[1]);
-                  operation = x -> x + number;
-                } catch (NumberFormatException e) {
-                  operation = x -> x + x;
-                }
-              }
+              return new Monkey(
+                  new ArrayList<>(items),
+                  operation,
+                  testDivisibleBy,
+                  trueMonkeyId,
+                  falseMonkeyId,
+                  new AtomicInteger());
+            })
+        .toList();
+  }
 
-              final long testDivisibleBy =
-                  parseLong(monkeyLines.get(3).substring("  Test: divisible by ".length()).trim());
-              final int trueMonkeyId =
-                  parseInt(
-                      monkeyLines
-                          .get(4)
-                          .substring("    If true: throw to monkey ".length())
-                          .trim());
-              final int falseMonkeyId =
-                  parseInt(
-                      monkeyLines
-                          .get(5)
-                          .substring("    If false: throw to monkey ".length())
-                          .trim());
+  private static LongFunction<Long> parseOperation(String operationLine) {
+    final String[] opString = operationLine.substring(23).split(" ");
 
-              monkeys.add(
-                  new Monkey(
-                      new ArrayList<>(items),
-                      operation,
-                      testDivisibleBy,
-                      trueMonkeyId,
-                      falseMonkeyId,
-                      new AtomicInteger()));
-            });
-    return monkeys;
+    LongFunction<Long> operation;
+
+    if ("*".equals(opString[0])) {
+      operation = "old".equals(opString[1]) ? x -> x * x : x -> x * parseLong(opString[1]);
+    } else {
+      operation = "old".equals(opString[1]) ? x -> x + x : x -> x + parseLong(opString[1]);
+    }
+    return operation;
   }
 }
